11084af59536a3014264b4274fd1c84e8e9f8bcb
Angel Zorrilla <whoangel.agl@gmail.com>
2026-05-24 19:24:20 -0600

    fix: update Cypress baseUrl to 4173 for vite preview

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "http://localhost:4173",
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     video: false,
